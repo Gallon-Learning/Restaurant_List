36be0f5044765039c2b8add294dae2167ac4dc57
GallonShih <Gallon.Shih@testritegroup.com>
2022-01-18 11:33:26 +0800

    feet: create new restaurant

--- app.js
+++ app.js
@@ -2,6 +2,8 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
+// 引用 body-parser
+const bodyParser = require('body-parser')
 
 const Restaurant = require('./models/restaurant')
 
@@ -27,20 +29,14 @@ app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 // setting static files
 app.use(express.static('public'))
+// 用 app.use 規定每一筆請求都需要透過 body-parser 進行前置處理
+app.use(bodyParser.urlencoded({ extended: true }))
 
 // routes setting
 app.get('/', (req, res ) => {
   Restaurant.find()
     .lean()
     .then((restaurantsData) => res.render('index', {restaurantsData}))
-})
-
-app.get('/restaurants/:id', (req, res ) => {
-  const id = req.params.id
-  return Restaurant.findById(id)
-    .lean()
-    .then((restaurantsData) => res.render('show', { restaurantsData }))
-    .catch(error => console.log(error))
 })
 
 app.get('/search', (req, res) => {
@@ -59,6 +55,36 @@ app.get('/search', (req, res) => {
       res.render("index", { restaurantsData: filterRestaurantsData, keyword })
     })
     .catch(err => console.log(err))
+})
+
+app.get('/restaurants/new', (req, res) => {
+  res.render("new")
+})
+
+app.post('/restaurants', (req, res) => {
+  const restaurant = req.body
+  return Restaurant.create({
+      'id': restaurant.id,
+      'name': restaurant.name,
+      'name_en': restaurant.englishName,
+      'category': restaurant.category,
+      'image': restaurant.image,
+      'location': restaurant.location,
+      'phone': restaurant.phone,
+      'google_map': restaurant.googleMap,
+      'rating': restaurant.rating,
+      'description': restaurant.description
+  })     // 存入資料庫
+    .then(() => res.redirect('/')) // 新增完成後導回首頁
+    .catch(error => console.log(error))
+})
+
+app.get('/restaurants/:id', (req, res ) => {
+  const id = req.params.id
+  return Restaurant.findById(id)
+    .lean()
+    .then((restaurantsData) => res.render('show', { restaurantsData }))
+    .catch(error => console.log(error))
 })
 
 // start and listen on the Express server
