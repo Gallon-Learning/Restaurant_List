534781e6f4397e08258504004113d2d74f352562
GallonShih <Gallon.Shih@testritegroup.com>
2022-03-23 16:40:38 +0800

    feat: add user login feature via passport

--- app.js
+++ app.js
@@ -4,8 +4,10 @@ const session = require('express-session')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
+const flash = require('connect-flash')
 
 const routes = require('./routes')
+const usePassport = require('./config/passport')
 require('./config/mongoose')
 const app = express()
 const port = 3000
@@ -21,6 +23,17 @@ app.use(session({
 }))
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
+usePassport(app)
+app.use(flash())
+app.use((req, res, next) => {
+  // 你可以在這裡 console.log(req.user) 等資訊來觀察
+  res.locals.isAuthenticated = req.isAuthenticated()
+  res.locals.user = req.user
+  res.locals.success_msg = req.flash('success_msg')  // 設定 success_msg 訊息
+  res.locals.warning_msg = req.flash('warning_msg')  // 設定 warning_msg 訊息
+  res.locals.error_msg = req.flash('error_msg')  // 設定 warning_msg 訊息
+  next()
+})
 app.use(routes)
 
 // start and listen on the Express server
