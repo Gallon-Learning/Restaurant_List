7a06d8c0f0278c461ba399432df209f64ab44c10
GallonShih <Gallon.Shih@testritegroup.com>
2022-03-23 17:33:32 +0800

    feat: add logout feature

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -59,4 +59,9 @@ router.post('/register', (req, res) => {
       .catch(err => console.log(err))
   })
 })
+router.get('/logout', (req, res) => {
+  req.logout()
+  req.flash('success_msg', '你已經成功登出。')
+  res.redirect('/users/login')
+})
 module.exports = router
